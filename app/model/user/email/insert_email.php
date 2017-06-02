3656a643982b86955f02593a0abb3469d1dc6635
Victor de la Fouchardiere <victor.dlf@outlook.fr>
2017-06-02 16:17:36 +0200

    correction créer un email dans la page template

--- app/model/user/email/insert_email.php
+++ app/model/user/email/insert_email.php
@@ -8,17 +8,17 @@
 		{
 			$req = "INSERT INTO mail_editor (id_user, 
 											email_dom,
-											template_name)
+											template_id)
 							VALUES (:id_user, 
 									:email_dom,
-									:template_name)";
+									:template_id)";
 
 			$query = $connexion->prepare($req);
 
 			// On initialise les valeurs
 			$query->bindValue(':id_user', $user, PDO::PARAM_INT);
 			$query->bindValue(':email_dom', $dom, PDO::PARAM_STR);
-			$query->bindValue(':template_name', $template, PDO::PARAM_STR);
+			$query->bindValue(':template_id', $template, PDO::PARAM_STR);
 
 			$query->execute();
 			
