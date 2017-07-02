bdacee05d68d176309611a71cecbfa5c905b0e45
Victor de la Fouchardière <victor.dlf@outlook.fr>
2017-07-02 19:54:19 +0200

    fixed bug mouse notask

--- app/view/layout/user/header.php
+++ app/view/layout/user/header.php
@@ -26,8 +26,8 @@
 		<?php } ?>
 
 		<!-- A DELETE !-->
-<!-- 		<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
- -->
+		<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
 	  	<!-- Lib [JS]-->
 		<script defer src="lib/js/jquery/jquery-3.1.1.min.js"></script>
 		<script defer src="lib/js/croppie/croppie-2.4.1.min.js"></script>
